1f6a4c216ced8b5a8d91b5ca6d99835374d25720
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-03-21 14:31:51 +0500

    styling in tailwind components layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,12 @@ export default async function Home() {
 
   return (
     <div className='bg-gray-300 min-h-screen flex justify-center items-center'>
-      <div className=" m-2 sm:m-4 md:m-8 md:p-32 sm:p-12 p-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className=" bookCardGrid">
         {booksList.map(({ id, type, name, avalible }: { id: string, type: string, name: string, avalible: boolean }) => (
-          <div key={id} className="bg-white text-center rounded-3xl flex flex-col w-full p-11 justify-between min-h-[50%]">
-            <div className='align-top text-2xl xl:text-3xl font-bold text-blue-500 '>{name}</div>
-            <div className='pt-6 pb-8 text-lg text-gray-800 font-semibold capitalize'>{type}</div>
-            <button className='text-lg text-gray-600 mx-auto hover:bg-blue-500 hover:text-white rounded-xl ring-2 ring-blue-600 py-1 px-2 '>Check Details</button>
+          <div key={id} className="bookCard">
+            <h1>{name}</h1>
+            <p>{type}</p>
+            <button>Check Details</button>
           </div>
         ))}
       </div>
