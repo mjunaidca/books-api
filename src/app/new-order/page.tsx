c4514750f2c175b02c3df47d2f1e11e3c50fdd9f
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-01 19:38:55 +0500

    Finalized Order Now Page

--- src/app/new-order/page.tsx
+++ src/app/new-order/page.tsx
@@ -1,12 +1,124 @@
-import React from "react";
-import OrderPage from "@/components/NewOrder";
+"use client";
+import { useState } from "react";
+import useSWR from "swr";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Loading from "@/app/loading";
+import { InputWithLabel } from "@/components/InputWithLabel";
+import { RingButton } from "@/components/Buttons/RingButton";
 
-const NewOrder = () => {
+const fetcher = async ({
+  bookId,
+  token,
+  clientName,
+  url,
+}: {
+  bookId: string;
+  token: string;
+  clientName: string;
+  url: string;
+}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ clientName: clientName, bookId: bookId }),
+  });
+
+  console.log("Token in fetcher:", token);
+  console.log("Response status:", response.status);
+
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the data");
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+const OrderPage = () => {
+  const [bookId, setbookId] = useState("");
+  const [callSWR, setCallSWR] = useState(false);
+  const [submitted, SetSubmitted] = useState(false);
+
+  const isClient = typeof window !== "undefined";
+  const bearer = isClient ? localStorage.getItem("accessToken") : null;
+  const name = isClient ? localStorage.getItem("name") : null;
+
+  const router = useRouter();
+
+  const { data, error } = useSWR(
+    callSWR
+      ? {
+          url: `/api/order`,
+          clientName: name,
+          bookId: bookId,
+          token: bearer,
+        }
+      : null,
+    fetcher
+  );
+
+  if (data) {
+    console.log("Order Token", data.orderId);
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log("Token before calling fetcher:", bookId);
+
+    setCallSWR(true);
+  };
+
   return (
-    <div>
-      <OrderPage />
+    <div className="min-h-screen flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white w-full max-w-lg p-8 rounded-xl shadow-md mx-4"
+      >
+        <h2 className="text-2xl sm:text-3xl font-semibold text-center mb-4 text-gray-800">
+          Order Confirmation
+        </h2>
+        {callSWR && (
+          <div className="SuccessText mb-8 text-center">
+            <p className="text-green-600 font-semibold mb-1">
+              Your Order Status:
+            </p>
+            {data ? (
+              `${(data.orderId && "Order Placed!") || data.error}`
+            ) : (
+              <Loading />
+            )}
+
+            <div className="flex items-center text-sm md:text-base mt-4 justify-center space-x-8">
+              <RingButton href="/" text="Browse Books" />
+              <RingButton href="/all-orders" text=" View Orders" />
+            </div>
+          </div>
+        )}
+
+        <InputWithLabel
+          label="Book ID"
+          value={bookId}
+          onChange={(e) => setbookId(e.target.value)}
+        />
+
+        <div className="flex items-center justify-center">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-8 rounded-lg focus:outline-none focus:shadow-outline transition-colors duration-200"
+            disabled={!bookId}
+          >
+            Place Order
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default NewOrder;
+export default OrderPage;
---
+++ src/components/Buttons/RingButton.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+export const RingButton = ({ href, text }: { href: string; text: string }) => {
+  return (
+    <div>
+        <Link
+          href={href}
+          className="px-6 py-2 bg-gray-200 ring-2 ring-blue-700 font-semibold rounded-lg hover:bg-blue-300 transition-colors duration-200"
+        >
+          {text}
+        </Link>
+    </div>
+  );
+};
+
